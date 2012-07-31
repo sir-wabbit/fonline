6089e97253762c80420955366ae047071f3919dd
Alexander Konovalov <alex.knvl@gmail.com>
2012-08-01 00:36:42 +0400

    Removed unnecessary files from client, added base/platform.hpp

---
+++ src/base/platform.hpp
@@ -0,0 +1,18 @@
+#ifndef FO_BASE_PLATFORM_HPP_
+#define FO_BASE_PLATFORM_HPP_
+
+namespace fonline {
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+typedef unsigned long long uint64;
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+typedef signed long long int64;
+
+}; // namespace fonline
+
+#endif//FO_BASE_PLATFORM_HPP_
