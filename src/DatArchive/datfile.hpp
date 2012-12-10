cb543a69d67e80fc01bd65a32d29bd891a12f088
Alexander Konovalov <alex.knvl@gmail.com>
2012-12-10 23:58:21 +0400

    Refactored DatArchive library, made it use stdio instead of winapi for file I/O

--- src/DatArchive/datfile.hpp
+++ src/DatArchive/datfile.hpp
@@ -3,6 +3,8 @@
 
 #include <map>
 #include <string>
+#include <stdio.h>
+#define __STDC_LIMIT_MACROS
 #include <stdint.h>
 
 #include "DatArchive.hpp"
@@ -19,13 +21,31 @@ enum
    ERR_FILE_NOT_SUPPORTED2,
 };
 
-typedef std::map<std::string, uint8_t*> index_map;
+typedef std::map<std::string, uint8_t*> IndexMap;
 
-struct IndexMap {
-	index_map index;
+struct DATArchiveInfo {
+  DATArchiveInfo() : FileSizeFromDat(0), TreeSize(0), FilesTotal(0), treePtr(0) { }
+
+  uint32_t FileSizeFromDat;
+  uint32_t TreeSize;
+  uint32_t FilesTotal;
+  
+  uint8_t* treePtr;
+  
+  IndexMap index;
 };
 
-typedef std::map<std::string, IndexMap> find_map;
+struct DATFileInfo {
+  DATFileInfo() : fileType(0), realSize(0), packedSize(0), offset(0) { }
+  // Eсли там 1, то файл считается компрессированым (не всегда).
+  uint8_t  fileType;
+  // Размер файла без декомпрессии.
+  uint32_t realSize;
+  // Размер сжатого файла.
+  uint32_t packedSize;
+  // Адрес файла в виде смещения от начала DAT-файла.
+  uint32_t offset;
+};
 
 class DatArchive {
 public:
@@ -35,40 +55,25 @@ public:
   DATARCHIVE_API bool Init(char* filename);
   DATARCHIVE_API bool IsLoaded();
 
-  DATARCHIVE_API int ReadTree();
-  DATARCHIVE_API void IndexingDAT();
-
   DATARCHIVE_API bool FindFile(const std::string& fname);
 
   DATARCHIVE_API bool DATOpenFile(char* fname);
   DATARCHIVE_API uint64_t DATGetFileSize();
   DATARCHIVE_API bool DATReadFile(void* buffer, size_t numberOfBytesToRead, size_t* numberOfBytesRead);
   DATARCHIVE_API bool DATSetFilePointer(int64_t offset, uint32_t moveMethod);
-
-  DATARCHIVE_API void ShowError();
   
   unsigned int error;
   
 private:
-  //index_map index;
-  find_map fmap;
-
-  std::string datFileName;
-
-  uint8_t  fileType; //если там 1, то файл считается компрессированым(не всегда).
-  uint32_t realSize; //Размер файла без декомпрессии
-  uint32_t packedSize; //Размер сжатого файла
-  uint32_t offset; //Адрес файла в виде смещения от начала DAT-файла.
+  int ReadTree();
+  void IndexingDAT();
+  
+  DATArchiveInfo archive;
+  DATFileInfo file;
 
   bool lError;
 
-  void* hFile; //Handles: (DAT) files
-
-  uint8_t *m_pInBuf;
-
-  uint32_t FileSizeFromDat;
-  uint32_t TreeSize;
-  uint32_t FilesTotal;
+  FILE* hFile; //Handles: (DAT) files
 
   uint8_t* ptr, *buff,*ptr_end;
   //in buff - DATtree, ptr - pointer
